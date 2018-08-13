941233da9ace13a82948f1408b19ccc4997c1d43
Hanad <hanad15@hotmail.co.uk>
2018-08-13 14:28:03 +0100

    grid is in and event listener setup

---
+++ index.js
@@ -0,0 +1,12 @@
+// load the jquery after the page has been loaded.
+$(document).ready(function(){
+player = 1;
+
+// event listener on the cells.
+  $(".cell").on("click", change)
+
+  function change() {
+    
+  }
+
+})
