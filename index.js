18fcddedf51299a621d6d046816da8a0dcf81138
Hanad <hanad15@hotmail.co.uk>
2018-08-13 22:16:29 +0100

    buttons will go to various pages

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // load the jquery after the page has been loaded.
 $(document).ready(function(){
-player = 1;
 
-// event listener on the cells.
-  $(".cell").on("click", change)
+$(".grid1").hide();
+$('h2').hide();
+$('p').hide();
 
-  function change() {
-    
+$('#Startgame').click(function(){
+  if ($(this).text().toLowerCase()== 'start game') {
+  $('button').hide();
+  $('.grid1').show();
+  $('h1').hide();
+  $('h2').hide();
+}
+})
+$('#Instructions').click(function(){
+  if ($(this).text().toLowerCase()== 'instructions') {
+    $('h2').show();
+    $('p').show();
+    $('button').hide();
+    $('.grid').hide();
+    $('h1').hide();
   }
+})
+
+
+
 
 })
