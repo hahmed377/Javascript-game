cb11c60d251fce9809ebe2ffcb54e887bfb245c0
Hanad <hanad15@hotmail.co.uk>
2018-08-14 08:29:37 +0100

    timer to start game added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 $(".grid1").hide();
 $('h2').hide();
 $('p').hide();
+$('#pageBeginCountdown').hide();
+$('h5').hide();
 
 $('#Startgame').click(function(){
   if ($(this).text().toLowerCase()== 'start game') {
@@ -11,6 +13,9 @@ $('#Startgame').click(function(){
   $('.grid1').show();
   $('h1').hide();
   $('h2').hide();
+  $('#pageBeginCountdown').show();
+  $('p').show();
+
 }
 })
 $('#Instructions').click(function(){
@@ -20,10 +25,27 @@ $('#Instructions').click(function(){
     $('button').hide();
     $('.grid').hide();
     $('h1').hide();
+
   }
 })
 
+ProgressCountdown(10, 'pageBeginCountdown', 'pageBeginCountdownText').then(value => alert(`Page has started: ${value}.`));
 
+function ProgressCountdown(timeleft, bar, text) {
+  return new Promise((resolve, reject) => {
+    var countdownTimer = setInterval(() => {
+      timeleft--;
+
+      document.getElementById(bar).value = timeleft;
+      document.getElementById(text).textContent = timeleft;
+
+      if (timeleft <= 0) {
+        clearInterval(countdownTimer);
+        resolve(true);
+      }
+    }, 1000);
+  });
+}
 
 
 })
